19e30085de511ddb1b19d7659283dd636898bda7
nightwatchcyber <research@nightwatchcybersecurity.com>
2019-07-10 20:26:25 -0400

    Fixing Travis file

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
